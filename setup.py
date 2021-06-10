10d13cf8a135ec5e80aaae04205145330485263a
Mohamed-512 <matex512@gmail.com>
2021-06-10 18:58:52 +0300

    Fix

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("extra_streamlit_components/README.md", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="extra_streamlit_components",
-    version="0.0.3",
+    version="0.0.8",
     author="Mohamed Abdou",
     author_email="matex512@gmail.com",
     description="An all-in-one place, to find complex or just not available components by default on streamlit.",
@@ -14,7 +14,11 @@ setuptools.setup(
     url="https://github.com/Mohamed-512/Extra-Streamlit-Components",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    classifiers=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     keywords=['Python', 'Streamlit', 'React'],
     python_requires=">=3.6",
     install_requires=[
