e445aad145ef4cc6ff0e7066db6011afdaea3c1f
Mohamed-512 <matex512@gmail.com>
2021-06-10 14:10:29 +0300

    pip package name update

--- Components/setup.py
+++ Components/setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    name="extra-streamlit-components",
-    version="0.0.1",
+    name="extra_streamlit_components",
+    version="0.0.3",
     author="Mohamed Abdou",
-    author_email="",
-    description="@Mohamed512a",
+    author_email="matex512@gmail.com",
+    description="An all-in-one place, to find complex or just not available components by default on streamlit.",
     long_description="",
     long_description_content_type="text/plain",
     url="https://github.com/Mohamed-512/Extra-Streamlit-Components",
@@ -15,9 +15,6 @@ setuptools.setup(
     keywords=['Python', 'Streamlit', 'React'],
     python_requires=">=3.6",
     install_requires=[
-        # By definition, a Custom Component depends on Streamlit.
-        # If your component has other Python dependencies, list
-        # them here.
         "streamlit >= 0.63",
     ],
 )
